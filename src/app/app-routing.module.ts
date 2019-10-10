3f7543d03033f66ec9ccb89be69aefa503747856
jasvinder <jasvinderlucky.singh@gmail.com>
2019-10-10 18:35:12 +0530

    code refactor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 
 import { RouteGuardService } from './service/route-guard.service';
-import { LogoutComponent } from './logout/logout.component';
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { ErrorComponent } from './error/error.component';
+import { ErrorComponent } from './shared/error/error.component';
 import { RegisterComponent } from './register/register.component';
-import { ListEmployeeComponent } from './list-employee/list-employee.component';
-import { EmployeeComponent } from './employee/employee.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
 
 
  
@@ -17,18 +15,29 @@ const routes: Routes = [
   { path: '', component: LoginComponent  },//canActivate, RouteGuardService
   { path: 'login', component: LoginComponent },
 
-  { path: 'employee', component: ListEmployeeComponent, canActivate:[RouteGuardService] },
+  { path: 'employee', component: EmployeeListComponent, canActivate:[RouteGuardService], children:[
+     
+     { path: ':empId', 
+       component: EmployeeEditComponent, 
+       canActivate:[RouteGuardService]
+     },
+
+  ] },
+  
   { path: 'welcome', component: WelcomeComponent, canActivate:[RouteGuardService] },
 
-  { path: 'logout', component: LogoutComponent, canActivate:[RouteGuardService] },
-  { path: 'employee/:empId', component: EmployeeComponent, canActivate:[RouteGuardService] },
+  
 { 
   path: 'register',
   component: RegisterComponent,
   canActivate: ["adminsOnlyGuard"]
 },
 
-
+{
+  path: 'error/:msg',
+  component:ErrorComponent,
+  canActivate:[RouteGuardService]
+},
   { path: '**', component: ErrorComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,25 @@ import { HttpIntercepterBasicAuthService } from './service/http/http-intercepter
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { ErrorComponent } from './error/error.component';
+import { ErrorComponent } from './shared/error/error.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
-import { LogoutComponent } from './logout/logout.component';
 import { RegisterComponent } from './register/register.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { ListEmployeeComponent } from './list-employee/list-employee.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { RbacAllowDirective } from './directive/rbac-allow.directive';
 import { Router } from '@angular/router';
-import { AuthorizationGuard } from './service/authorization.guard';
+import { AuthorizationGuard } from './service/auth/authorization.guard';
+import { RoleComponent } from './register/role/role.component';
+import { RbacAllowDirective } from './shared/directive/rbac-allow.directive';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
 
 
 export function createAdminOnlyGuard(router:Router) {
@@ -33,17 +36,21 @@ export function createAdminOnlyGuard(router:Router) {
     ErrorComponent,
     MenuComponent,
     FooterComponent,
-    LogoutComponent,
     RegisterComponent,
     EmployeeComponent,
     ListEmployeeComponent,
     WelcomeComponent,
-    RbacAllowDirective
+    RbacAllowDirective,
+    RoleComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [
