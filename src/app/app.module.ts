81660b56010f6a5db0b949e03cc0b1aa53b93a80
jasvinder <jasvinderlucky.singh@gmail.com>
2019-10-10 18:41:10 +0530

    app module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,18 @@ import { ErrorComponent } from './shared/error/error.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { RegisterComponent } from './register/register.component';
-import { EmployeeComponent } from './employee/employee.component';
-import { ListEmployeeComponent } from './list-employee/list-employee.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { Router } from '@angular/router';
 import { AuthorizationGuard } from './service/auth/authorization.guard';
 import { RoleComponent } from './register/role/role.component';
 import { RbacAllowDirective } from './shared/directive/rbac-allow.directive';
-import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
-import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
 import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 
 
 export function createAdminOnlyGuard(router:Router) {
   return new AuthorizationGuard(['ADMIN'],  router);
 }
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,14 +31,11 @@ export function createAdminOnlyGuard(router:Router) {
     MenuComponent,
     FooterComponent,
     RegisterComponent,
-    EmployeeComponent,
-    ListEmployeeComponent,
+    EmployeeEditComponent,
+    EmployeeListComponent,
     WelcomeComponent,
     RbacAllowDirective,
-    RoleComponent,
-    EmployeeListComponent,
-    EmployeeDetailComponent,
-    EmployeeEditComponent
+    RoleComponent
   ],
   imports: [
     BrowserModule,
